5182d5294a180cc04d03f61dbcd265e179569727
Jamir Hernandez <21-77830@g.batstate-u.edu.ph>
2025-05-04 15:34:55 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -694,8 +694,7 @@ mysqli_close($conn);
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<!-- Include jQuery BEFORE your script -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 
 
 <script>
